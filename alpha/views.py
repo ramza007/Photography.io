62d1eff5e42f424e543e560b535d40e1286549cb
ramza007 <ramzalvo007@gmail.com>
2019-04-11 21:57:46 +0300

    created static and urls, and added styling and major updates

--- alpha/views.py
+++ alpha/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.template import RequestContext
 
 # Create your views here.
+def index(request):
+    title = "Ramza  | Home"
+    return render(request, 'index.html', {"title": title})
