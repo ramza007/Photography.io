629779a518981c074b0a6261d0e436135629ab77
ramza007 <ramzalvo007@gmail.com>
2019-04-15 22:58:40 +0300

    changed banner name and started work on the footer

--- alpha/views.py
+++ alpha/views.py
@@ -3,5 +3,5 @@ from django.template import RequestContext
 
 # Create your views here.
 def index(request):
-    title = "Ramza  | Home"
+    title = "Ramza Ombati"
     return render(request, 'index.html', {"title": title})
