796594904dc59f365e62b07388f28ca42bb67da5
ramza007 <ramzalvo007@gmail.com>
2019-06-22 23:30:08 +0300

    removed article and tags models

--- alpha/admin.py
+++ alpha/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from .models import Editor, Article, Tags
+from .models import Editor
 
 # Registered models here.
 
 admin.site.register(Editor)
-admin.site.register(Article)
-admin.site.register(tags)
