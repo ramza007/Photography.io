77d8f8acf5e4bd97e8aede549df36dda7a9ab056
ramza007 <ramzalvo007@gmail.com>
2019-08-04 17:11:06 +0300

    - Changed database setting

--- omega/settings.py
+++ omega/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfolio_alpha',
-        'USER': 'user',
+        'USER': 'ramza',
         'PASSWORD': 'ramza123',
     }
 }
