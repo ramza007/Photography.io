72f88ec79368a944822b7eb52c44999923a18af2
ramza007 <ramzalvo007@gmail.com>
2020-02-09 14:33:59 -0500

    - Modified static storage from settings

--- omega/settings.py
+++ omega/settings.py
@@ -134,6 +134,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
 MEDIA_URL = '/media/'
