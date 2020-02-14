cdb69fcfc88dc71a68ab0b8d099aede1376654b8
ramza007 <ramzalvo007@gmail.com>
2020-02-13 21:54:05 -0500

    - Changed STATICFILES STORAGE provider

--- omega/settings.py
+++ omega/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 
 MEDIA_URL = '/media/'
