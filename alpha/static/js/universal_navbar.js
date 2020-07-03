2c8dfe765032587954c7151f72131e38f4f0436f
ramza007 <ramzalvo007@gmail.com>
2020-07-02 23:59:20 -0400

    *********** DESIGN CHANGES *********** - Modified index stylesheet to accomodate for smaller screens - Replaced oldstyle navbar template with hamburger menu and sidebar for smaller devices, and corresponding stylesheet - Modified other navbars with similar properies and their corresponding stylesheets - Introduced Universal JavaScript file for navbar, to be used later

---
+++ alpha/static/js/universal_navbar.js
@@ -0,0 +1,18 @@
+function openNav() {
+    var x = document.getElementById("mySidebar");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
