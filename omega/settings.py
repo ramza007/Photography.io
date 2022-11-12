5128d19b8f65d3f069c05d1dfd896e352da0c425
Ramsa <ramzalvo007@gmail.com>
2022-11-12 13:50:48 -0500

    - Moved secret key into env file - Begining work on optimizing site for mobile

--- omega/settings.py
+++ omega/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#713x&$744+lnvw7@$xengk8y0z9p%wv-1l17vf!^xk8tg=2z&'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
