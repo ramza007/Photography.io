5d1f1fe750c85bde19c4137c7ba68ffcc4050e71
Ramsa <ramzalvo007@gmail.com>
2023-04-16 09:09:10 -0400

    - Modified setting for deployment

--- omega/settings.py
+++ omega/settings.py
@@ -29,17 +29,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-ALLOWED_HOSTS = []
-
 # For testing purposes 
 # DEBUG = False
 # ALLOWED_HOSTS = ['*']
 
 # for production
-# DEBUG = False
-# ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'photography-io.herokuapp.com']
+DEBUG = False
+ALLOWED_HOSTS = ['www.ramsa.studio', 'ramsa.studio']
 
 # Application definition
 
