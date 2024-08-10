ddb68af9e9ed46eb0d42b4d5bd8fe15e0e0dc108
ramsa-codeserver <ramzalvo007@gmail.com>
2024-08-10 20:27:13 +0000

    - Minor modifications for production

--- omega/settings.py
+++ omega/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # For Local Testing
 DEBUG = True
-ALLOWED_HOSTS = ["django.ramza.space"]
+ALLOWED_HOSTS = ["django.ramza.space", "ramsa.studio", "www.ramsa.studio"]
 CSRF_TRUSTED_ORIGINS =["https://django.ramza.space"]
 
 
